1c3a97a0c8237b561b1db4dbcdff18f0ec8b31e3
Aritra Mustafi <aritramustafi56@gmail.com>
2026-07-27 00:07:10 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int low = 0, start = 0, len = INT_MAX, count = 0;
+        int m = t.size();
+        int n = s.size();
+        map<char, int> mp;
+        for(char x: t){
+            mp[x]++;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(mp[s[i]] > 0){
+                count++;
+            }
+            mp[s[i]]--;
+            if(count == m){
+                while(low < i && mp[s[low]] < 0){
+                    mp[s[low]]++;
+                    low++;
+                }
+
+                if(len > i - low){
+                    len = i - low + 1;
+                    start = low;
+                }
+                mp[s[low]]++;
+                low++;
+                count--;
+            }
+        }
+        return len == INT_MAX ? "" : s.substr(start, len);
+    }
+};
